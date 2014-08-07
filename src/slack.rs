16d27dffc7db9d233ebe463b1bb757f61753d92b
Christopher Brickley <chris@frostly.com>
2014-08-06 22:51:06 -0400

    update pub field

--- src/slack.rs
+++ src/slack.rs
@@ -2,8 +2,8 @@ use curl::http;
 use serialize::{json, Encodable};
 
 pub struct Slack {
-    domain : &'static str,
-    token  : &'static str
+    pub domain : &'static str,
+    pub token  : &'static str
 }
 
 impl Slack {
@@ -20,29 +20,29 @@ impl Slack {
 
 #[deriving(Encodable, Show)]
 pub struct Payload {
-    channel      : &'static str,
-    text         : &'static str,
-    username     : Option<&'static str>,
-    icon_url     : Option<&'static str>,
-    icon_emoji   : Option<&'static str>,
-    attachments  : Option<Attachments>,
-    unfurl_links : Option<u8>,
-    link_names   : Option<u8>
+    pub channel      : &'static str,
+    pub text         : &'static str,
+    pub username     : Option<&'static str>,
+    pub icon_url     : Option<&'static str>,
+    pub icon_emoji   : Option<&'static str>,
+    pub attachments  : Option<Attachments>,
+    pub unfurl_links : Option<u8>,
+    pub link_names   : Option<u8>
 }
 
 #[deriving(Encodable, Show)]
 pub struct Attachments {
-    fallback : &'static str,
-    text     : Option<&'static str>,
-    pretext  : Option<&'static str>,
-    color    : &'static str,
-    fields   : Vec<Attachment>
+    pub fallback : &'static str,
+    pub text     : Option<&'static str>,
+    pub pretext  : Option<&'static str>,
+    pub color    : &'static str,
+    pub fields   : Vec<Attachment>
 }
 
 #[deriving(Encodable, Show)]
 pub struct Attachment {
-    title : &'static str,
-    value : &'static str,
-    short : Option<bool>
+    pub title : &'static str,
+    pub value : &'static str,
+    pub short : Option<bool>
 }
 
