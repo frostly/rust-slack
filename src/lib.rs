1e10a24aba0f9a0cb3fd9bac9434446d1c0b7742
Christopher Brickley <chris@frostly.com>
2014-09-06 18:10:49 -0400

    handle slack text and slack links

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 #![crate_name = "slack"]
 #![feature(phase)]
+extern crate regex;
+#[phase(plugin)] extern crate regex_macros;
 #[phase(plugin, link)] extern crate log;
 extern crate curl;
 extern crate serialize;
-
-pub use slack::{Slack, Payload, Attachment, Attachments};
+pub use slack::{Slack, Payload, Attachment, Field, SlackText, SlackLink};
 
 mod slack;
--- src/slack.rs
+++ src/slack.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use curl::http;
 use std::str;
 use serialize::{json, Encodable};
@@ -30,33 +31,76 @@ impl Slack {
 #[deriving(Encodable, Show)]
 pub struct Payload {
     pub channel      : String,
-    pub text         : String,
+    pub text         : SlackText,
     pub username     : Option<String>,
     pub icon_url     : Option<String>,
     pub icon_emoji   : Option<String>,
-    pub attachments  : Option<Attachments>,
+    pub attachments  : Option<Attachment>,
     pub unfurl_links : Option<u8>,
     pub link_names   : Option<u8>
 }
 
 #[deriving(Encodable, Show)]
-pub struct Attachments {
-    pub fallback : String,
-    pub text     : Option<String>,
-    pub pretext  : Option<String>,
+pub struct Attachment {
+    pub fallback : SlackText,
+    pub text     : Option<SlackText>,
+    pub pretext  : Option<SlackText>,
     pub color    : String,
-    pub fields   : Vec<Attachment>
+    pub fields   : Option<Vec<Field>>
 }
 
 #[deriving(Encodable, Show)]
-pub struct Attachment {
+pub struct Field {
     pub title : String,
-    pub value : String,
+    pub value : SlackText,
     pub short : Option<bool>
 }
 
+#[deriving(Encodable)]
+pub struct SlackText(String);
+
+impl fmt::Show for SlackText {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let SlackText(ref text) = *self;
+        let mut re = regex!("&");
+        let mut t2 = re.replace_all(text.as_slice(), "&amp;");
+        re = regex!("<");
+        t2 = re.replace_all(t2.as_slice(), "&lt;");
+        re = regex!(">");
+        t2 = re.replace_all(t2.as_slice(), "&gt;");
+        write!(f , "{}" , t2)
+    }
+}
+
+#[deriving(Encodable)]
+pub struct SlackLink {
+    pub url: String,
+    pub text: SlackText,
+}
+
+impl fmt::Show for SlackLink {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f , "<{}|{}>" , self.url , self.text)
+    }
+}
+
 #[test]
-fn slack_test() {
+fn slack_incoming_url_test() {
     let s = Slack::new("hello.com".to_string(), "secret".to_string());
     assert_eq!(s.incoming_url, "https://hello.com.slack.com/services/hooks/incoming-webhook?token=secret".to_string());
+}
+
+#[test]
+fn slack_text_test() {
+    let s = SlackText("moo <&> moo".to_string());
+    assert_eq!(format!("{}",s), "moo &lt;&amp;&gt; moo".to_string());
+}
+
+#[test]
+fn slack_link_test() {
+    let s = SlackLink {
+        text: SlackText("moo <&> moo".to_string()),
+        url: "http://google.com".to_string()
+    };
+    assert_eq!(format!("{}",s), "<http://google.com|moo &lt;&amp;&gt; moo>".to_string());
 }
